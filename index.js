fdbfea59cf237935d703b279233733284859ed38
Lay Hunt <tianxiemaochiyu@outlook.com>
2021-02-22 16:31:49 +0800

    add a milestone for stars

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const { Octokit } = require('@octokit/rest')
 
 const checkInterval = 5000 // ms
 
+// emoji
+const milestone = [
+    ":broken_heart:",
+    ":heart:",
+    ":orange_heart:",
+    ":purple_heart:",
+    ":yellow_heart:",
+    ":heartpulse:",
+    ":sparkling_heart:",
+    ":gift_heart:",
+    ":heartbeat:",
+    ":two_hearts:",
+    ":revolving_hearts:" 
+]
+
 const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN,
 })
@@ -13,10 +28,11 @@ const getRepoStars = () => {
     }).then(repo => repo.data.stargazers_count)
 }
 
-const renameRepo = (name) => {
+const renameRepo = ({ name, description }) => {
     return octokit.request('PATCH /repositories/:id', {
         id: process.env.REPO_ID,
         name,
+        description
     })
 }
 
@@ -27,7 +43,10 @@ const checkAndRenameRepo = async () => {
         const stars = await getRepoStars()
 
         if (lastStars !== stars) {
-            await renameRepo(`This-Repo-Has-${stars}-Stars`)
+            await renameRepo({
+                name: `This-Repo-Has-${stars}-Stars`,
+                description: `Yes, it's true ${getMilestone(starts)}`
+            })
 
             lastStars = stars
         }
@@ -36,6 +55,11 @@ const checkAndRenameRepo = async () => {
     }
 
     setTimeout(checkAndRenameRepo, checkInterval)
+}
+
+const getMilestone = (stars) => {
+    let emojiIndex = parseInt(parseInt(stars) / 500);
+    return milestone[emojiIndex >= milestone.length ? milestone.length - 1 : emojiIndex ];
 }
 
 getRepoStars()
